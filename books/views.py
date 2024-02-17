443b50154d68e890fe50a66c3f5dfde1716380f6
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 15:36:07 +0530

    defined BooksAPI

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Books
+from .serializers import BooksSerializer
 
 # Create your views here.
+
+
+class BooksAPI(ModelViewSet):
+    serializer_class = BooksSerializer
+    queryset = Books.objects.all()
+    permission_classes = (IsAdminUser,)
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
