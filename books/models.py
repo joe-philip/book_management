8399d25c4dfbb544a3a01c4f409428f8aa11be1e
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 15:35:34 +0530

    defined Books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from root.utils.fields import CommonFields
+
 # Create your models here.
+
+
+class Books(CommonFields):
+    title = models.CharField()
+    author = models.ForeignKey('author.Authors', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'books'
+        verbose_name = 'Book'
+
+    def __str__(self) -> str: return self.title
