534ec2905258b81aacd7ade7a6bfa6695fe5758b
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 17:21:49 +0530

    defined URL for ReviewsAPI

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+router = DefaultRouter()
+
+router.register('review', views.ReviewsAPI, basename='review')
 
 urlpatterns = [
     path('signup', views.SignupAPI.as_view()),
     path('login', views.LoginAPI.as_view())
-]
+]+router.urls
