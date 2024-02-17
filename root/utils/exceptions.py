d1b5cb04ddf25ee7cb0913a1e07fc06b823c18fd
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 14:20:38 +0530

    modified exception handler

--- root/utils/exceptions.py
+++ root/utils/exceptions.py
@@ -2,7 +2,7 @@ from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
 
-def exception_handler(exc: Exception, **context) -> Response:
+def exception_handler(exc: Exception, *args, **context) -> Response:
     if isinstance(exc, APIException):
         return Response(exc.detail, status=exc.status_code)
     return Response(str(exc), status=500)
