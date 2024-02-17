24e98f24bb20596119812528e6160e755c94cc0d
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 15:02:12 +0530

    defined AuthorsAPI

--- author/views.py
+++ author/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Authors
+from .serializers import AuthorsSerializer
+
 
 # Create your views here.
+class AuthorsAPI(ModelViewSet):
+    queryset = Authors.objects.all()
+    serializer_class = AuthorsSerializer
+    permission_classes = (IsAdminUser,)
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
