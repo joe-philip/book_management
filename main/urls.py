4eb0a62e66becf2b4ac60a803b7b145e3e58dc2f
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 19:31:21 +0530

    defined URL for AuthorsListAPI

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,6 @@ router.register('review', views.ReviewsAPI, basename='review')
 urlpatterns = [
     path('signup', views.SignupAPI.as_view()),
     path('login', views.LoginAPI.as_view()),
-    path('books', views.BooksListAPI.as_view())
+    path('books', views.BooksListAPI.as_view()),
+    path('authors', views.AuthorsListAPI.as_view())
 ]+router.urls
