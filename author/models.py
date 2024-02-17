e06e0071aa702ce5efcbedf28a39380b52ece6de
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 18:39:53 +0530

    modified Authors model
    added new property average_rating

--- author/models.py
+++ author/models.py
@@ -7,6 +7,19 @@ from root.utils.fields import CommonFields
 class Authors(CommonFields):
     name = models.CharField()
 
+    @property
+    def average_rating(self) -> float:
+        from main.models import Reviews
+
+        ratings = Reviews.objects.filter(
+            content_type__app_label='author',
+            content_type__model='authors',
+            object_id=self.id
+        ).values_list('rating', flat=True)
+        if ratings.count() != 0:
+            return sum(ratings)/ratings.count()
+        return 0
+
     class Meta:
         db_table = 'author'
         verbose_name = 'Author'
