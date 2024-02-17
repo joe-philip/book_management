8e3fd6ea361cdab142fca726cf26fe19ed01e663
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 15:02:26 +0530

    defined URL for AuthorsAPI

--- author/urls.py
+++ author/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
-urlpatterns = []
+router = DefaultRouter()
+
+router.register('', views.AuthorsAPI, basename='authors')
+
+urlpatterns = []+router.urls
