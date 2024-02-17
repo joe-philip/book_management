6aac24cb67050fb8c38c4fc18f0a144f69b50343
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 11:33:42 +0530

    added config for postgresql database

--- root/settings.py
+++ root/settings.py
@@ -85,10 +85,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
