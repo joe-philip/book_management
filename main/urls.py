847714b4281249e1b69ced9758706317115c0694
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 13:45:25 +0530

    defined URL for LoginAPI

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.SignupAPI.as_view())
+    path('signup', views.SignupAPI.as_view()),
+    path('login', views.LoginAPI.as_view())
 ]
