03dc6bc49ea17b7f9089cefbe45f1ac872526ef9
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 18:40:08 +0530

    modified Books model
    added new property average_rating

--- books/models.py
+++ books/models.py
@@ -9,6 +9,18 @@ class Books(CommonFields):
     title = models.CharField()
     author = models.ForeignKey('author.Authors', on_delete=models.CASCADE)
 
+    @property
+    def average_rating(self) -> float:
+        from main.models import Reviews
+
+        ratings = Reviews.objects.filter(
+            content_type__app_label='books', content_type__model='books',
+            object_id=self.id
+        ).values_list('rating', flat=True)
+        if ratings.count() != 0:
+            return sum(ratings)/ratings.count()
+        return 0
+
     class Meta:
         db_table = 'books'
         verbose_name = 'Book'
