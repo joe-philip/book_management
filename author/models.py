af7ff6e350c90dd520f38fd0e7d03dba4e8763e6
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 14:42:43 +0530

    defined Authors model

--- author/models.py
+++ author/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from root.utils.fields import CommonFields
+
+
 # Create your models here.
+class Authors(CommonFields):
+    name = models.CharField()
+
+    class Meta:
+        db_table = 'author'
+        verbose_name = 'Author'
+
+    def __str__(self) -> str: return self.name
