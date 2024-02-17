41b32f6a1977962cee54ce789cea819dc41071cc
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 12:55:30 +0530

    defined SignupAPI

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+from .serializers import SignupSerializer
 
 # Create your views here.
+
+
+class SignupAPI(CreateAPIView):
+    serializer_class = SignupSerializer
