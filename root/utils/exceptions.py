207993ce19588d1362f469808b33a1c108bb5612
Joe Philip <joe.philip@hotmail.co.in>
2024-02-17 15:07:09 +0530

    modified exception handler

--- root/utils/exceptions.py
+++ root/utils/exceptions.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
@@ -5,4 +6,6 @@ from rest_framework.response import Response
 def exception_handler(exc: Exception, *args, **context) -> Response:
     if isinstance(exc, APIException):
         return Response(exc.detail, status=exc.status_code)
+    if isinstance(exc, Http404):
+        return Response('Object not found', status=404)
     return Response(str(exc), status=500)
